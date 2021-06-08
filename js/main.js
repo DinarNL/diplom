5825c5a1fb928c501c27e232eec44e6c9f9d6bf6
NDL <dinarsafin196@gmail.com>
2021-06-08 16:34:03 +0400

    init project

---
+++ js/main.js
@@ -0,0 +1,137 @@
+$(document).ready(() => {
+
+    $('.header-item').click(function () {
+        $('.header-item').removeClass('active');
+        $(this).addClass('active');
+    });
+
+
+    $('.fourth-block-slick-content').slick({
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 3,
+        variableWidth: false,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 501,
+                settings: {
+                    centerMode: true,
+                    centerPadding: '0px',
+                    slidesToShow: 1,
+                    variableWidth: false,
+                    dots: true
+                }
+            }
+        ]
+    });
+
+
+    $('.fourth-block-slick-content').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+        $(".slick-slide").removeClass('slick-fix');
+        if (currentSlide === slick.slideCount - 1) {
+            $(".fourth-block-slick-content").find("[data-slick-index='" + currentSlide + "']").next().addClass('slick-fix');
+        }
+    });
+
+
+    $('.open').click(() => {
+        $('#pop-up-discount-container').css('display', 'flex');
+    });
+
+    $('#pop-up-discount-cancel, #pop-up-discount-container').click((e) => {
+        if (e.target.id === 'pop-up-discount-cancel' || e.target.id === 'pop-up-discount-container' || e.target.id === 'pop-up-discount-cancel-image') {
+            $('#pop-up-discount-container').hide();
+        }
+    });
+
+
+
+    $('.open-block ').click(() => {
+        $('#pop-up-check-container').css('display', 'flex');
+    });
+
+    $('#pop-up-check-cancel, #pop-up-check-container').click((e) => {
+        if ( e.target.id === 'pop-up-check' || e.target.id === 'pop-up-check-cancel' || e.target.id === 'pop-up-check-cancel-image') {
+            $('#pop-up-check-container').hide();
+        }
+    });
+
+
+    $('#submit').click(function () {
+        $('.pop-up-check-block-info-error').hide();
+        let name = $('#name');
+        let services = $('#services');
+        let data = $('#data');
+        let number = $('#number');
+        let man = $('#man');
+        let time = $('#time');
+        let hasError = false;
+
+        if (!name.val()) {
+            name.siblings('.pop-up-check-block-info-error').show();
+            hasError = true;
+            $(name).css('border-color', 'red');
+        } else {
+            $(name).css('border-color', 'rgb(185, 145, 80)');
+        }
+        if ($("#services option:selected").text()==="Выберете услугу") {
+            services.siblings('.pop-up-check-block-info-error').show();
+            hasError = true;
+            $(services).css('border-color', 'red');
+        } else {
+            $(services).css('border-color', 'rgb(185, 145, 80)');
+        }
+        if (!data.val()) {
+            data.siblings('.pop-up-check-block-info-error').show();
+            hasError = true;
+            $(data).css('border-color', 'red');
+        } else {
+            $(data).css('border-color', 'rgb(185, 145, 80)');
+        }
+        if (!number.val()) {
+            number.siblings('.pop-up-check-block-info-error').show();
+            hasError = true;
+            $(number).css('border-color', 'red');
+        } else {
+            $(number).css('border-color', 'rgb(185, 145, 80)');
+        }
+        if ($("#man option:selected").text()==="Выберете мастера") {
+            man.siblings('.pop-up-check-block-info-error').show();
+            hasError = true;
+            $(man).css('border-color', 'red');
+        } else {
+            $(man).css('border-color', 'rgb(185, 145, 80)');
+        }
+        if ($("#time option:selected").text()==="Выберете время") {
+            time.siblings('.pop-up-check-block-info-error').show();
+            hasError = true;
+            $(time).css('border-color', 'red');
+        } else {
+            $(time).css('border-color', 'rgb(185, 145, 80)');
+        }
+
+        if (hasError===false) {
+            $.ajax({
+                type: 'post',
+                url:'mail.php',
+                data: 'name=' + name.val() +'&services=' + services.val() + '&data=' + data.val() + '&number' + number.val() + '&man' + man.val() + '&time' + time.val(),
+                success: () => {
+                    $('#pop-up-check-container').hide();
+                    $('#pop-up-thanks').css('display', 'flex');
+                },
+                error: () => {
+                    $('#pop-up-check-container').hide();
+                    alert('Ошибка бронирования. Свяжитесь пожалуйста по намеру телефона. ')
+                }
+            });
+        }
+    });
+
+    $('#pop-up-thanks-close, #pop-up-thanks-container').click((e) => {
+        if ( e.target.id === 'pop-up-thanks-close' || e.target.id === 'pop-up-thanks-container' || e.target.id === 'pop-up-thanks-image') {
+            $('#pop-up-thanks').hide();
+        }
+    });
+
+});
---
+++ mail.php
@@ -0,0 +1,37 @@
+<?php
+// Проверяем тип запроса, обрабатываем только POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Получаем параметры, посланные с javascript
+    $name = $_POST['name'];
+    $services = $_POST['services'];
+    $data = $_POST['data'];
+    $number = $_POST['number'];
+    $man = $_POST['man'];
+    $time = $_POST['time'];
+
+    // создаем переменную с содержанием письма
+    $content = $name . ' записался к мастеру ' . $man . ', на услугу ' . $services . ' в ' . $time . ',' . $data . 'года. Его номер: ' . $number;
+
+    $headers = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+
+
+    // Первый параметр - кому отправляем письмо, второй - тема письма, третий - содержание
+    $success = mail("admin@barbershop.com ", 'Запрос на услугу', $content, $headers);
+
+    if ($success) {
+        // Отдаем 200 код ответа на http запрос
+        http_response_code(200);
+        echo "Письмо отправлено";
+    } else {
+        // Отдаем ошибку с кодом 500 (internal server error).
+        http_response_code(500);
+        echo "Письмо не отправлено";
+    }
+
+} else {
+    // Если это не POST запрос - возвращаем код 403 (действие запрещено)
+    http_response_code(403);
+    echo "Данный метод запроса не поддерживается сервером";
+}
